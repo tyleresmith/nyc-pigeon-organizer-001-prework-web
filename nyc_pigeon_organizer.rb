755779aaa6c82b84f853bc0a5e437033a03d8dac
tyleresmith <oats000@hotmail.com>
2016-02-16 01:14:13 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  h = {}
+  
+  data.each do |stats, values|
+    values.each do |value, pigeons|
+      pigeons.each do |pigeon|
+        h[pigeon] = {color: [], gender: [], lives: []}
+      end
+    end
+  end
+  data.each do |stats, values|
+    values.each do |value, pigeons|
+      pigeons.each do |pigeon|
+        h[pigeon][stats] << value.to_s
+      end
+    end
+  end
+  h
 end
